1d43241170dbf0d147abe4adee25e3ba31e4d39c
soqueroprogramar <joao.morais.jumper@hotmail.com>
2016-12-22 11:57:02 +0000

    Função do ponto médio bem feita. (não alterar mais, melhor que isto não fica)

--- src/III/met_III.m
+++ src/III/met_III.m
@@ -3,10 +3,9 @@ function y = met_III(a, b, y0, n, f)
   h = (b-a)/n; %n is the number of subintervals
   y = [y0];      %y are our approximations
   
-  for i = a:h:b
+  for i = a:h:(b-h)
     y = [y (y(end) + h * f(i + h/2, y(end) + h/2*f(i, y(end))))];
   endfor
   
-
 
 endfunction
