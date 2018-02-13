2e8296626bfa604db2ca7f05a2ba043cdafa9047
João Pinheiro <joao.castropinheiro@gmail.com>
2018-02-13 03:03:02 +0000

    Add plotting code

--- src/III/c.m
+++ src/III/c.m
@@ -1,8 +1,4 @@
-
-hold on;
-xlabel('Tempo');
-ylabel('Pessoas');
-grid on;
+clear; clc; close all;
 
 a = 0;
 b = 50;
@@ -16,13 +12,23 @@ R = @(t, s) -300*log(s) + 2005.008;
 
 s_t = ponto_medio(a, b, 799, intervals, S);
 
-plot(x_range, s_t, ';i)   S - Susceptiveis;', 'color', 'green', 'linewidth', 2);
-plot(x_range, I(0, s_t), ';ii)  I - Infectados;', 'color', 'red', 'linewidth', 2);
-plot(x_range, R(0, s_t) , ';iii) R - Recuperados;', 'color', 'blue', 'linewidth', 2);
+f = figure();
+hold on;
+xlabel('Tempo');
+ylabel('Pessoas');
+grid on;
+
+plot(x_range, s_t, ';i)   S - Susceptiveis;', 'color', 'green', 'linewidth', 4);
+plot(x_range, I(0, s_t), ';ii)  I - Infectados;', 'color', 'red', 'linewidth', 4);
+plot(x_range, R(0, s_t) , ';iii) R - Recuperados $\lambda\alpha$  ;', 'color', 'blue', 'linewidth', 4);
 
 l = legend('location', 'east');
 legend boxon;
-fontsize=15;
-set(l, 'FontSize', 13);
+fontsize=14;
+set(l, 'FontSize', fontsize);
 set(l, 'color', 'w');
 set([gca; findall(gca, 'Type','text')], 'FontSize', fontsize);
+
+% Generate a .tex for text and .pdf for graphics, compile .tex with pdflatex and
+% include the resulting pdf as a figure in the original latex
+print(f, '-dpdflatexstandalone', 'test');
